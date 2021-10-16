b96414745b3df2cd1e007738aa7281afeac064f7
Home <yulaguby@gmail.com>
2021-10-16 11:52:14 +0300

    Initial commit

---
+++ src/pro/sky/lesson01/lesson01.java
@@ -0,0 +1,8 @@
+package pro.sky.lesson01;
+
+public class lesson01 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/pro/sky/lesson03/homeWork03.java
@@ -0,0 +1,58 @@
+package pro.sky.lesson03;
+
+public class homeWork03 {
+    public homeWork03() {
+    } public static void main(String[] args) {
+        boolean clientOS = false;
+        if (!clientOS) {
+            System.out.println("Установите приложение для iOS по ссылке");
+        } else if (clientOS) {
+            System.out.println("Установите приложение для Android по ссылке");
+        }
+        int clientDeviceYear = 2010;
+        if (clientDeviceYear < 2015 && !clientOS) {
+            System.out.println("Установите облегчённую версию для iOS по ссылке:");
+        } else if (clientDeviceYear < 2015 && clientOS) {
+            System.out.println("Установите облегчённую версию для Android по ссылке");
+        }
+        int year = 2021;
+        if ((year % 4 != 0 || year % 100 == 0) && year % 400 != 0) {
+            System.out.println(year + " Год не является високосным");
+        } else {
+            System.out.println(year + " Год является високосным");
+        }
+        int deliveryDistance = 100;
+        int takeDays = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Даставка займёт " + takeDays + " день");
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Даставка займёт " + (takeDays + 1) + " дня");
+        } else if (deliveryDistance <= 100) {
+            System.out.println("Даставка займёт " + (takeDays + 2) + " дня");
+        }
+        int monthNumber = 13;
+        switch(monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+            default:
+                System.out.println("Такого дня не существует");
+        }
+    }
+}
