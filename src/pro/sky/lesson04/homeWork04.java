ebb012cd529a72d3b0a6334f72b020c25c3cd93c
Home <yulaguby@gmail.com>
2021-10-16 12:35:10 +0300

    04

--- src/pro/sky/lesson04/homeWork04.java
+++ src/pro/sky/lesson04/homeWork04.java
@@ -1,4 +1,12 @@
 package pro.sky.lesson04;
 
 public class homeWork04 {
+
+    public static void main(String[] args) {
+        boolean clientOS = false;
+        if (!clientOS) {
+            System.out.println("Установите приложение для iOS по ссылке");
+        } else if (clientOS) {
+            System.out.println("Установите приложение для Android по ссылке");
+        }
 }
